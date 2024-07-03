a13b4c5c5d18b8dfb90bd486b9eff53d6418882c
AReeme <areeme@student.neumont.edu>
2024-07-03 08:10:40 -0600

    Polished Up NPC AI

--- Ouroboros/Assets/Scripts/Museum/NPCAI.cs
+++ Ouroboros/Assets/Scripts/Museum/NPCAI.cs
@@ -8,16 +8,18 @@ public class NPCAI : MonoBehaviour
     NavMeshAgent agent;
     [SerializeField] LayerMask groundLayer;
 
-    //Patrol
+    // Patrol
     Vector3 destination;
     bool walkPointSet;
     [SerializeField] float range;
+    bool isMovingToDestination;
+    float startTime; 
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
+        agent.speed = 1.0f;
     }
 
     // Update is called once per frame
@@ -28,22 +30,53 @@ public class NPCAI : MonoBehaviour
 
     void Patrol()
     {
-        if (!walkPointSet)  SearchForDest();
+        if (!walkPointSet && !isMovingToDestination)
+        {
+            StartCoroutine(WaitAndSearch());
+            SearchForDest();
+        }
 
-        if (walkPointSet) agent.SetDestination(destination);
-        if (Vector3.Distance(transform.position, destination) < 10) walkPointSet = false;
+        if (walkPointSet)
+        {
+            agent.SetDestination(destination);
+            startTime = Time.time;
+        }
+
+        if (Vector3.Distance(transform.position, destination) < 1)
+        {
+            walkPointSet = false;
+            isMovingToDestination = false;
+            Debug.Log("Reached destination");
+        }
+        else if (Time.time - startTime > 5 && !isMovingToDestination) 
+        {
+            SearchForDest(); 
+        }
     }
 
     void SearchForDest()
     {
-        float z = Random.Range(-range, range);
-        float x = Random.Range(-range, range);
+        float x;
+        float z;
 
-        destination = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+        do
+        {
+            x = Random.Range(-range, range);
+            z = Random.Range(-range, range);
+        } while ((x >= -5 && x <= 5) || (z >= -5 && z <= 5));
+
+        destination = new Vector3(transform.position.x + x + 50, transform.position.y, transform.position.z + z + 50);
 
         if (Physics.Raycast(destination, Vector3.down, groundLayer))
         {
             walkPointSet = true;
+            isMovingToDestination = true;
         }
+    }
+
+    IEnumerator WaitAndSearch()
+    {
+        yield return new WaitForSeconds(10);
+        SearchForDest();
     }
 }
