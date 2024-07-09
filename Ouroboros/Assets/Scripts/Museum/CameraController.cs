f9158323426bf1babca652d9f30ae40df13ce417
epiccreeper10 <87875284+epiccreeper10@users.noreply.github.com>
2024-07-08 18:57:30 -0600

    Created Character Controller In world

--- Ouroboros/Assets/Scripts/Museum/CameraController.cs
+++ Ouroboros/Assets/Scripts/Museum/CameraController.cs
@@ -1,28 +1,44 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 public class CameraController : MonoBehaviour
 {
     public float lookSpeed = 2.0f;
     private Vector2 rotation = Vector2.zero;
 
+    private XRNode inputSource;
+
     private InputAction lookAction;
+    private bool isVR;
 
     void Awake()
     {
         // Initialize the look action
         lookAction = new InputAction(type: InputActionType.Value, binding: "<Mouse>/delta");
         lookAction.Enable();
+
+        // Check if the application is running in VR mode
+        isVR = XRSettings.isDeviceActive;
     }
 
     void Update()
     {
-        Vector2 lookInput = lookAction.ReadValue<Vector2>();
-        rotation.x += lookInput.x * lookSpeed;
-        rotation.y -= lookInput.y * lookSpeed;
-        rotation.y = Mathf.Clamp(rotation.y, -90f, 90f);
+        isVR = true;
+        if (!isVR)
+        {
 
-        transform.localRotation = Quaternion.Euler(rotation.y, rotation.x, 0);
+            Vector2 lookInput = lookAction.ReadValue<Vector2>();
+            rotation.x += lookInput.x * lookSpeed;
+            rotation.y -= lookInput.y * lookSpeed;
+            rotation.y = Mathf.Clamp(rotation.y, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(rotation.y, rotation.x, 0);
+        }
+        else
+        {
+            
+        }
     }
 
     void OnEnable()
