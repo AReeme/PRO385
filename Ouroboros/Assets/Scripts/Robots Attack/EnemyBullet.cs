cea5f6d98bcb7e0ec05760f0b1ce4c18aba5de3c
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2024-08-05 20:04:31 -0600

    created enemy bullet script

---
+++ Ouroboros/Assets/Scripts/Robots Attack/EnemyBullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+	float bulletDamage = 5;
+	[SerializeField] private float speed = 20f;
+	[SerializeField] private float lifetime = 5f;
+
+	void Start()
+	{
+		Destroy(gameObject, lifetime);
+	}
+
+	void Update()
+	{
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+	void OnColliderEnter(Collider other)
+	{
+		//player.health -= bulletDamage
+		// Destroy(this.gameObject);
+	}
+}
